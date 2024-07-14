d2105c4ca91e6545b84591a6583384a776fb514b
Sowdarjya <kolaysowdarjya@gmail.com>
2024-07-14 19:29:30 +0530

    Fetched data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 async function fetchData() {
   let data = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=`
   );
   let response = await data.json();
   console.log(response);
