cbe3e1caaf50f2a61d2db1d616a5350c1935d00c
Sowdarjya <kolaysowdarjya@gmail.com>
2024-07-14 21:37:26 +0530

    added the fetched to the dom and styled it

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-async function fetchData() {
+const input = document.querySelector("input");
+const searchBtn = document.getElementById("search-btn");
+const recipeList = document.querySelector(".items");
+
+async function fetchData(query) {
   let data = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=`
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
   );
   let response = await data.json();
-  console.log(response);
+  recipeList.innerHTML = "";
+  response.meals.forEach((element) => {
+    console.log(element);
+    const recipe = document.createElement("div");
+    recipe.classList.add("item");
+    recipe.innerHTML = `
+    <img src=${element.strMealThumb}>
+    <h3>${element.strMeal}</h3>
+    <h4>${element.strArea}</h4>
+    <p>Category: ${element.strCategory}</p>
+    `;
+    recipeList.appendChild(recipe);
+  });
 }
 
-fetchData();
+searchBtn.addEventListener("click", () => {
+  fetchData(input.value);
+});
+
+window.onload = fetchData(input.value);
