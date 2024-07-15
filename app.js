ff4edb663aaa92b8b3f598fd9b16c6613f7d2e54
Sowdarjya <kolaysowdarjya@gmail.com>
2024-07-15 13:00:58 +0530

    created pop up menu for recipes

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const input = document.querySelector("input");
 const searchBtn = document.getElementById("search-btn");
 const recipeList = document.querySelector(".items");
+const recipeDescription = document.querySelector(".recipes");
+const ingredientsList = document.createElement("ul");
+const instructions = document.createElement("p");
+
+function recipePopUp(element) {
+  ingredientsList.innerHTML = "";
+  instructions.innerHTML = "";
+  recipeDescription.innerHTML = `
+  <h2>${element.strMeal}</h2>
+  <h3>Ingredients required</h3>`;
+  for (let i = 1; i <= 20; i++) {
+    ingredient = element[`strIngredient${i}`];
+    if (ingredient) {
+      let li = document.createElement("li");
+      const measure = element[`strMeasure${i}`];
+      li.textContent = `${ingredient} - ${measure}`;
+      ingredientsList.appendChild(li);
+    }
+  }
+  const h3 = document.createElement("h3");
+  h3.textContent = "Instructions";
+  recipeDescription.appendChild(ingredientsList);
+  instructions.textContent = `${element.strInstructions}`;
+  recipeDescription.appendChild(h3);
+  recipeDescription.appendChild(instructions);
+  console.log(ingredientsList);
+  recipeDescription.style.display = "block";
+}
 
 async function fetchData(query) {
   let data = await fetch(
@@ -17,8 +45,16 @@ async function fetchData(query) {
     <h3>${element.strMeal}</h3>
     <h4>${element.strArea}</h4>
     <p>Category: ${element.strCategory}</p>
-    <button>Learn More</button>
     `;
+
+    const btn = document.createElement("button");
+    recipe.appendChild(btn);
+    btn.textContent = "Check recipe";
+
+    btn.addEventListener("click", () => {
+      recipePopUp(element);
+    });
+
     recipeList.appendChild(recipe);
   });
 }
